8981548ee53d46f0274e284ce61ac4d14d7c775f
Mark Gatere <gateremark5@gmail.com>
2023-02-14 00:22:03 +0300

    Editted the createItemEl for each column function

--- script.js
+++ script.js
@@ -104,19 +104,19 @@ function updateDOM() {
 	// Progress Column
 	progressListEl.textContent = "";
 	progressListArray.forEach((progressItem, index) => {
-		createItemEl(progressListEl, 0, progressItem, index);
+		createItemEl(progressListEl, 1, progressItem, index);
 	});
 	progressListArray = filterArray(progressListArray);
 	// Complete Column
 	completeListEl.textContent = "";
 	completeListArray.forEach((completeItem, index) => {
-		createItemEl(completeListEl, 0, completeItem, index);
+		createItemEl(completeListEl, 2, completeItem, index);
 	});
 	completeListArray = filterArray(completeListArray);
 	// On Hold Column
 	onHoldListEl.textContent = "";
 	onHoldListArray.forEach((onHoldItem, index) => {
-		createItemEl(onHoldListEl, 0, onHoldItem, index);
+		createItemEl(onHoldListEl, 3, onHoldItem, index);
 	});
 	onHoldListArray = filterArray(onHoldListArray);
 	// Run getSavedColumns only once, Update Local Storage
